f2fa8607cf223efefb2b07936efe6c160eff978c
郭威成 <wilson@Wilson-MBA.local>
2016-10-14 10:21:31 +0800

    自訂設定變數、新增login方法及其view

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$config['webname'] = "測試網站";
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
---
+++ application/views/welcome_login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="#">
+		<p>帳號：<input type="text" name="user" size="20"></p>
+		<p>密碼：<input type="password" name="pass" size="20"></p>
+		<p><input type="submit" name="send" value="送出"></p>
+	</form>
+</body>
+</html>
